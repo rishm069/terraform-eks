7b084d18cc9dc516530d8c92dbb853c0e07e1660
r.ishmukhametov <r.ishmukhametov@platform.live>
2022-11-23 22:48:42 +0400

    base nginx added

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -7,6 +7,9 @@ module "eks" {
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+  node_security_group_tags = {
+  "kubernetes.io/cluster/${local.cluster_name}" = null
+  }
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
